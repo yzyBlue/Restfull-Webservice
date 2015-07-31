be6d46169cbbe90c014a0a71bfd1e544c7f7fded
yzyBlue <bluedelta9192@gmail.com>
2015-07-31 13:53:05 +0800

    添加delete和update操作方法

--- ssm-example/src/main/java/com/test/ssm/controller/UserController.java
+++ ssm-example/src/main/java/com/test/ssm/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,21 +19,42 @@ import javax.annotation.Resource;
 @RequestMapping("/user")
 public class UserController {
 	@Resource(name = "userDao")
-	private UserDao UserDao;
+	private UserDao userDao;
 	protected final Logger logger =Logger.getLogger(this.getClass()) ;
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET,produces = "application/xml;charset=UTF-8")
 	public @ResponseBody User queryUserDataById(@PathVariable("id") String id){
 		logger.info("查询   User 信息");
-		User user=UserDao.getUser(Integer.valueOf(id));
+		User user=userDao.getUser(Integer.valueOf(id));
 		return user;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET,produces = "application/xml;charset=UTF-8")
 	public @ResponseBody UserList queryAllUserData(){
 		logger.info("查询   User 信息");
-		List<User> user=UserDao.getAllUser();
+		List<User> user=userDao.getAllUser();
 		UserList users=new UserList();
 		users.setUserlist(user);
 		return users;
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/{id}",produces = "application/xml;charset=UTF-8")
+	public @ResponseBody User updateUserInfo(@PathVariable String id, @RequestBody User user){
+		logger.info(" update User 信息  : "+user.toString());
+		int i=userDao.updateUser(user);
+		logger.info("查询   User 信息");
+		User usernew=userDao.getUser(user.getId());
+		return usernew;
+	}
+	@RequestMapping(method=RequestMethod.DELETE,value="/{id}",produces = "application/xml;charset=UTF-8")
+	public @ResponseBody String deleteUserInfo(@PathVariable String id){
+		logger.info(" delete User 信息   ");
+		int i=userDao.deleteUser(Integer.parseInt(id));
+		String response="";
+		if(i>0){
+			response="SUCCESS";
+		}else{
+			response="FAILED";
+		}
+		return response;
 	}
 }
--- ssm-example/src/main/java/com/test/ssm/model/UserList.java
+++ ssm-example/src/main/java/com/test/ssm/model/UserList.java
@@ -2,12 +2,13 @@ package com.test.ssm.model;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="userList")
 public class UserList {
 	private List<User> userlist;
-
+	@XmlElement(name="user")
 	public List<User> getUserlist() {
 		return userlist;
 	}
